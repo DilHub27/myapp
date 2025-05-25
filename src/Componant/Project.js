035463992e8e8348840154c4e948bc7bf406dc86
Dilshan Udaraka <durdilshan29@gmail.com>
2025-05-25 11:37:11 +0530

    Your message here, e.g., Updated homepage design

--- src/Componant/Project.js
+++ src/Componant/Project.js
@@ -18,25 +18,25 @@ export default function Project() {
           </p>
           <div className="grid grid-cols-2 gap-4">
             <img
-               src={`${process.env.PUBLIC_URL}/p2.jpg`} alt="..." />
+              src="/p2.jpg"
               alt="startup template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
             <img
-               src={`${process.env.PUBLIC_URL}/p4.jpg`} alt="..." />
+              src="p4.jpg"
               alt="startup template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
             <img
-              src={`${process.env.PUBLIC_URL}/p1.jpg`} alt="..." />
+              src="p1.jpg"
               alt="startup template"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />
             <img
-              src={`${process.env.PUBLIC_URL}/p3.jpg`} alt="..." />
+              src="p3.jpg"
               alt="startup template"
               width={500}
               height={500}
